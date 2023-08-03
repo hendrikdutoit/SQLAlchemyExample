5ac636bbe65db6ab98f3887fa35b5f9cf326fd64
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-03 17:22:20 +0200

    Updated READ.ME

--- push.bat
+++ push.bat
@@ -1,1 +1,4 @@
+CALL del_dt.bat
 CALL gitit adda
+CALL gitit commitcust -m %1
+CALL gitit pushall
---
+++ release.bat
@@ -0,0 +1,5 @@
+CALL del_dt.bat
+CALL git commit --allow-empty -m "Release %1"
+CALL git tag -a %1.%2 -m "Version %1"
+CALL git push --tags
+CALL git checkout master
