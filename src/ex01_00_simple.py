66c2fc6cb5e89cc9c3a86ccb060eba0d691140fb
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-06 00:43:54 +0200

    Split into different schemas

--- src/ex01_00_simple.py
+++ src/ex01_00_simple.py
@@ -1,11 +1,31 @@
 from os import environ
 from sqlalchemy import Column, Integer, String, Identity
-import db_connection
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 
-Base = db_connection.Base
-engine = db_connection.engine
-session = db_connection.session
+# Base = db_connection.Base
+# engine = db_connection.engine
+# session = db_connection.session
+
+url = engine.URL.create(
+    "mysql+mysqlconnector",
+    username="root",
+    password=environ.get("MYSQL_ROOT_PWD"),
+    host=environ.get("MYSQL_HOST"),
+    port=environ.get("MYSQL_TCP_PORT_EXAMPLES"),
+    database=environ.get("MYSQL_DB_NAME"),
+)
+# url = 'sqlite:///:memory:'
+engine = create_engine(url, echo=False)
+if database_exists(engine.url):
+    drop_database(engine.url)
+create_database(engine.url)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base(bind=engine)
 
 
 class Person(Base):
