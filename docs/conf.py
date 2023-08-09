8afe882b6923740bfdc8daca2039be5528ee4ade
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-09 04:34:22 +0200

    Updated ReadTheDocs config files

--- docs/conf.py
+++ docs/conf.py
@@ -11,14 +11,14 @@ html_context = {
     "github_user": "hendrikdutoit",  # Username
     "github_repo": "SQLAlchemyExample",  # Repo name
     "github_version": "master",  # Version
-    "conf_py_path": "/source",  # Path in the checkout to the docs root
+    "conf_py_path": "source",  # Path in the checkout to the docs root
 }
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary']
-templates_path = ['_templates']
+templates_path = ['source/_templates']
 language = 'en'
 exclude_patterns = []
 html_theme = 'bizstyle'
-html_static_path = ['static']
+html_static_path = ['_static']
 autosummary_generate = True
 autosummary_imported_members = True
 master_doc = 'source/index'
