bf94401d1dfdfb6e5edee22514e9252db85f5e3b
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-09 17:03:55 +0200

    Reset ReadTheDocs configuration to defualt

---
+++ docs.1/conf.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.insert(0, '/src')
+project = 'SQLAlchemyExample'
+copyright = '2022, Hendrik du Toit'
+author = 'Hendrik du Toit'
+version = '0'
+release = '0.0.1'
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "hendrikdutoit",  # Username
+    "github_repo": "SQLAlchemyExample",  # Repo name
+    "github_version": "master",  # Version
+    "conf_py_path": "source",  # Path in the checkout to the docs root
+}
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary']
+templates_path = ['source/_templates']
+language = 'en'
+exclude_patterns = []
+html_theme = 'bizstyle'
+html_static_path = ['_static']
+autosummary_generate = True
+autosummary_imported_members = True
+master_doc = 'source/index'
