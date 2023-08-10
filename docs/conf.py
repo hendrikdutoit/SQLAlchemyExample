f4933bad4d221de6e9cd30e9dffd6736547b0837
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-10 14:52:47 +0200

    Configure ReadTheDocs parameters

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@
 project = 'SQLAlchemyExample'
 copyright = '2023, Hendrik du Toit'
 author = 'Hendrik du Toit'
-release = '0.1.20'
-version = '0.1.20'
+release = '0.1.21'
+version = '0.1.21'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
