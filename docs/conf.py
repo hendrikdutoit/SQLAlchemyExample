32eea66a8f0dbb5eb2bff8ff10211b8ba428c52e
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-14 02:15:12 +0200

    API contents ommitted

--- docs/conf.py
+++ docs/conf.py
@@ -18,12 +18,12 @@ import sys
 project = 'SQLAlchemyExample'
 copyright = '2023, Hendrik du Toit'
 author = 'Hendrik du Toit'
-release = '0.1.38'
-version = '0.1.38'
+release = '0.1.39'
+version = '0.1.39'
 
 add_module_names = False
-sys.path.insert(0, os.path.abspath(r'./src'))
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(r'../src'))
+# sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 print('Hello world!"')
 print(sys.path)
