9e82b5482e8ad374eeb404c97bf0eb5202912418
Hendrik du Toit <hendrik@brightedge.co.za>
2024-05-07 23:27:15 +0200

    Resolve Black and reorder-python-imports incompatibility

--- install.ps1
+++ install.ps1
@@ -2,6 +2,7 @@ Write-Host "Running D:\Dropbox\Projects\HdT\sqlalchemyexample\install.ps1..." -F
 pip install --upgrade --force --no-cache-dir black
 pip install --upgrade --force --no-cache-dir flake8
 pip install --upgrade --force --no-cache-dir pre-commit
+pip install --upgrade --force --no-cache-dir isort
 pre-commit install
 pre-commit autoupdate
 if (Test-Path -Path $env:PROJECT_DIR\pyproject.toml) {pip install --no-cache-dir -e .[dev]}
